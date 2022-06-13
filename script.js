e7a68d9c28f4aaebac4a5403ee84a9ed7ab9a20c
dayanelaura <dayanelaura3@gmail.com>
2022-06-13 17:25:08 -0300

    js add mas

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+function selecionar(elemento, selecao){
+    const clicado = document.querySelector('.${selecao} .borda');
+  
+    if(clicado !== null)
+         clicado.classList.remove("borda");
+    
+    elemento.classList.add("borda");
 
-    // const elemento = document.querySelector(".topo");
-	// alert(elemento.innerHTML);
+    ativaricone();
+    finalizarpedido();
+} 
+
+let prato = null; 
+let bebida = null; 
+let sobremesa = null;
+
+function finalizarpedido(){
+
+   if(prato !== null && bebida !== null && sobremesa !== null){ 
+        const confirmar = document.querySelector("button");
+        confirmar.innerHTML = "Fechar Pedido";
+        confirmar.classList.add("botaoverde");
+    }   
+}
+
+function ativaricone(elemento, selecao){
+    const clicado = document.querySelector('.${selecao} .iconeverde'); 
+    const icone = elemento.querySelector(".iconeverde");
+
+    if(clicado !== null)
+        clicado.classList.remove("iconeverde");
+    
+    icone.classList.add("iconeverde");
+}
